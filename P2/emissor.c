a352dbfc6901a83086ec7904be8162a3cb29f8a7
Hugo <hugomguima@gmail.com>
2020-10-06 19:55:56 +0100

    Started State machine on sender

--- P2/emissor.c
+++ P2/emissor.c
@@ -12,7 +12,58 @@
 #define FALSE 0
 #define TRUE 1
 
+#define FLAG 0x7e
+#define A 0x03
+#define C_SET 0x03
+#define C_UA 0x07
+
+
+#define ERROR -1
+
 volatile int STOP=FALSE;
+
+enum state {START,FLAG_RCV,A_RCV,C_RCV,BCC_OK,STOP};
+enum state current = START;
+
+int receiverInteraction(int serialPort){
+
+    char c; // char reaad. Changes the state
+
+    //Colocar aqui o código da espera pelo byte
+
+
+    //State Machine
+    switch(current){
+        case START:
+            if(c == FLAG){
+                puts("Flag Received");
+                current = FLAG_RCV;
+            }
+            break;
+        case FLAG_RCV:
+            if(c == A){
+                puts("A Received");
+                current = A_RCV;
+            }
+            else if(c == FLAG);
+            else{
+                current = START;
+            }
+            break;
+        case A_RCV:
+            if(c == C_SET) current = C_RCV;
+            break;
+        case C_RCV:
+          break;
+        case BCC_OK:
+          break;
+        case STOP:
+          break;
+        default:
+          break;
+  }
+
+}
 
 int main(int argc, char** argv)
 {
