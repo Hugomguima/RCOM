6805ee420ed3cac993ac6bd9ebdb4e4001186996
pedrovponte <up201809694fe.up.pt>
2020-10-06 19:50:44 +0100

    Start recetor

--- P2/emissor.c
+++ P2/emissor.c
@@ -7,19 +7,52 @@
 #include <stdio.h>
 
 #define BAUDRATE B38400
-#define MODEMDEVICE "/dev/ttyS1"
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
 
+#define FLAG 0x7E
+#define A 0x03
+#define C_SET 0x03
+#define C_UA 0x07
+
 volatile int STOP=FALSE;
+
+void waitSetMachine(int fd) {
+  tcflush(fd, TCIOFLUSH); //limpa informacao recebida mas nao lida e informacao escrita mas nao transmitida
+
+  enum state {START,FLAG_RCV,A_RCV,C_RCV,BCC_OK,STOP};
+  enum state current = START;
+
+  int finish = FALSE;
+
+  while(finish == FALSE) {
+    
+    switch(current){
+      case START:
+        break;
+      case FLAG_RCV:
+        break;
+      case A_RCV:
+        break;
+      case C_RCV:
+        break;
+      case BCC_OK:
+        break;
+      case STOP:
+        break;
+      default:
+        break;
+    }
+  }
+
+}
 
 int main(int argc, char** argv)
 {
   int fd,c;
   struct termios oldtio,newtio;
-  char *buf = NULL;
-  int i, sum = 0, speed = 0;
+  char message[4096], byte;
 
   if ( (argc < 2) || 
         ((strcmp("/dev/ttyS0", argv[1])!=0) && 
@@ -29,12 +62,12 @@ int main(int argc, char** argv)
   }
 
 
-  /*
+/*
   Open serial port device for reading and writing and not as controlling tty
   because we don't want to get killed if linenoise sends CTRL-C.
-  */
+*/
 
-
+  
   fd = open(argv[1], O_RDWR | O_NOCTTY );
   if (fd <0) {perror(argv[1]); exit(-1); }
 
@@ -56,10 +89,10 @@ int main(int argc, char** argv)
 
 
 
-  /* 
+/* 
   VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
   leitura do(s) pr�ximo(s) caracter(es)
-  */
+*/
 
 
 
@@ -70,77 +103,52 @@ int main(int argc, char** argv)
     exit(-1);
   }
 
-  printf("New termios structure set\n");
+  printf("Waiting for a message...\n");
+
+  ssize_t res;
+  size_t num_bytes_read = 0;
 
 
+  while (1) {       /* loop for input */
+    res = read(fd, &byte, 1);
 
-  /*for (i = 0; i < 255; i++) {
-    buf[i] = 'a';
-  }
-
-
-  buf[25] = '\n';*/
-
-  size_t n;
-  ssize_t n_bytes;
-
-  printf("Enter a message: ");
-
-  n_bytes = getline(&buf, &n, stdin);
-
-  if(n_bytes == -1) {
-    fprintf(stderr, "Error in reading from stdin");
-    exit(-2);
-  }
-
-  buf[n_bytes - 1] = '\0';
-
-  n_bytes = write(fd,buf,n_bytes * sizeof(buf[0]));   
-
-  if(n_bytes == -1) {
-    fprintf(stderr, "Failed to write in serial port");
-    exit(-3);
-  }
-
-  printf("%zd bytes written\n", n_bytes);
-
-
-  /* 
-  O ciclo FOR e as instru��es seguintes devem ser alterados de modo a respeitar 
-  o indicado no gui�o 
-  */
-
-  char message[4096];
-  char byte;
-  n = 0;
-
-  while(1) {
-    n_bytes = read(fd, &byte, 1);
-
-    if(n_bytes == -1) {
-      fprintf(stderr, "Error reading return message");
-      exit(-4);
+    if(res == -1) {
+      fprintf(stderr, "Error on reading from serial port \n" );
+      exit(-3);
     }
 
-    message[n++] = byte;
-
+    message[num_bytes_read++] = byte;
+    
     if(byte == '\0') {
       break;
     }
   }
 
-  printf("\nWaiting for a message...\n");
-  printf("Message returned: %s\n", message);
+  printf("\n%zd bytes read from the serial port\n", num_bytes_read);
+  printf("Message read: %s\n", message);
 
-  sleep(2);
+  printf("Sending message...\n");
 
-  free(buf);
+  res = write(fd, message, strlen(message)+1);
 
-  if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
-    perror("tcsetattr");
-    exit(-1);
+  if(res == -1) {
+    fprintf(stderr, "Error writing to serial port\n");
+    exit(-3);
   }
 
+  else if(res != strlen(message) + 1) {
+    fprintf(stderr, "Error: could not send the whole message!\n");
+    exit(-4);
+  }
+
+  printf("Message sent\n");
+
+/* 
+  O ciclo WHILE deve ser alterado de modo a respeitar o indicado no gui�o 
+*/
+
+  sleep(2);
+  tcsetattr(fd,TCSANOW,&oldtio);
   close(fd);
   return 0;
 }
