ab1ab9835c0ef335aef4eed663832c7b43e0bc1f
pedrovponte <up201809694@fe.up.pt>
2020-10-24 19:00:34 +0100

    Finish llread and llclose (receiver)

--- P3/macros.h
+++ P3/macros.h
@@ -10,11 +10,16 @@
 #define A_ER 0x01 //campo de endereco em comandos enviados pelo recetor e repostas enviadas pelo emissor
 #define C_SET 0x03
 #define C_UA 0x07
+#define C_DISC 0x0B
 #define NS0 0x00 //valor de ns na primeira trama do write
 #define Ns1 0x40 //valor de ns segunda trama do write
 #define ESCAPE_BYTE 0x7d 
 #define ESCAPE_FLAG 0x5e
 #define ESCAPE_ESCAPE 0x5d
+#define RR0 0x05
+#define RR1 0x85
+#define REJ0 0x01
+#define REJ1 0x81
 
 #define TRANSMITTER 0 
 #define RECEIVER 1
