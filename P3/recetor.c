a76b143865ae26b94fdef9a55a73685524b1219f
Hugo <hugomguima@gmail.com>
2020-10-31 17:12:17 +0000

    Revert "Merge branch 'new' into main"
    This reverts commit 9f67f41b5d89351d73678b48690de22464ca2ccf.

--- P3/recetor.c
+++ P3/recetor.c
@@ -1,11 +1,9 @@
 #include "recetor.h"
 
-extern unsigned int packetNumber;
-
 int main(int argc, char** argv)
 {
   int fd;
-  off_t index = 0;
+  unsigned int size;
   //unsigned char message[4096], byte;
 
   if ( (argc < 2) || 
@@ -23,89 +21,16 @@ int main(int argc, char** argv)
   fd = open(argv[1], O_RDWR | O_NOCTTY );
   if (fd <0) {perror(argv[1]); exit(-1); }
 
-  if(llopen(fd, RECEIVER) == ERROR){
-    puts("Error on LLOPEN");
-    return -3;
-  }
+  llopen(fd, RECEIVER);
 
-  unsigned char* start;
-  unsigned int size, sizeStart;
+  unsigned char* buffer = malloc(0);
+  size = llread(fd,buffer);
 
-  size = llread(fd,start);
-  sizeStart = size;
+  //write(fd,buffer,size); // Aqui deve escrever a mesagem do llread
 
-  unsigned int *fileSize = 0;
-  unsigned int *nameSize = 0;
-  unsigned char *fileName  = (unsigned char *)malloc(0);
+  llclose(fd,RECEIVER);
 
-  if(checkStart(start,fileSize,fileName,nameSize) == ERROR){
-    puts("Error on checkStart");
-    return -5;
-  }
-
-  // Loop for reading all llwrites from the emissor
-
-  unsigned char* message = malloc(0); // Creates null pointer to allow realloc
-  unsigned char* dataPacket;
-  unsigned int packetsReaded = 0;
-  unsigned int messageSize;
-
-  unsigned char* result = (unsigned char*)malloc(*fileSize);
-
-  while(TRUE){
-    unsigned int *packetSize;
-    messageSize = 0;
-    
-    if(messageSize = llread(fd,message) == -1){
-      puts("Error on llread data packet ");
-      exit(-1);
-    }
-    
-    if(message[0] == CT_END){
-      puts("Reached Control End Packet");
-      break;
-    }
-    
-    packetsReaded++;
-    
-    printf("Received packet number: %d\n", packetsReaded);
-
-    dataPacket = assembleDataPacket(message,messageSize,packetSize);
-
-    for(int i= 0; i < *packetSize; i++){
-      result[index + i] = dataPacket[i];
-    }
-
-    index += *packetSize;
-
-    free(dataPacket);
-  }
-
-  if(checkEND(start, sizeStart, message, messageSize) == 1) {
-    puts("Start and End packets are different!");
-    exit(-1);
-  }
-
-  printf("Received a file of size: %u\n", *fileSize);
-
-  // Displaying all the message after the protocol is implemented
-  for(int i = 0; i < *fileSize; i++){
-    printf("%x",result[i]); // Não si se aqui é suposto meter em hexadecimal ou deixo estar como unsigned char :/ 
-  }
-
-  // Creating the file to be rewritten after protocol
-  createFile(result,*fileSize,fileName);
-
-
-  if(llclose(fd, RECEIVER) == ERROR){
-    puts("Error on LLCLOSE");
-    return -3;
-  }
-
-  sleep(2);
-
-  free(fileName);
-  free(result);
+  sleep(1);
 
   close(fd);
 
