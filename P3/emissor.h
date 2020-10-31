9c988fbd380a598bab46845d485cc692eb738f79
Hugo <hugomguima@gmail.com>
2020-10-31 17:23:33 +0000

    Added headers

---
+++ P3/emissor.h
@@ -0,0 +1,22 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+
+#include <errno.h>
+#include <signal.h>
+#include <stdlib.h>
+
+#include <string.h>
+
+#include "llfunctions.h"
+#include "application.h"
+
+
+/**
+ * \brief main function that starts the proggram flow
+ * @param argc argument count
+ * @param argv char pointer array with the arguments
+ */
+int main(int argc, char** argv);
---
+++ P3/recetor.h
@@ -0,0 +1,14 @@
+/*Non-Canonical Input Processing*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <string.h>
+
+
+// Created files
+
+#include "llfunctions.h"
+#include "application.h"
