8bed3d1d79ae28a72d5aaa85a7b09b60d0b635ba
Hugo <hugomguima@gmail.com>
2020-10-31 17:09:19 +0000

    Revert "Merge branch 'main' into ApplicationLayer"
    This reverts commit 054d831b8354fd58fbecfb271d1f235c9a402215.

--- P3/llfunctions.c
+++ P3/llfunctions.c
@@ -62,12 +62,11 @@ int llopen(int fd, int status) {
             alarm(5); // Call an alarm to wait for the message
 
             if(receiveUA(fd) == TRUE){
-                printf("UA received\n");
+                printf("Interaction received\n");
                 STP = TRUE;
                 counter = 0;
                 alarm(0);
             }
-            //sleep(1);
 
         }while(STP == FALSE && counter < MAXTRIES);
     }
@@ -79,7 +78,7 @@ int llopen(int fd, int status) {
                 return -1;
             }
             else {
-                printf("Sent UA message\n");
+                printf("Send UA message\n");
             }
         }
         else {
@@ -92,11 +91,11 @@ int llopen(int fd, int status) {
 
 unsigned char getBCC2(unsigned char *mensagem, int size){
 
-    unsigned char bcc2 = mensagem[0];
-    
-    for(int i = 1; i < size; i++){
+    unsigned char bcc2;
+    for(int i = 0; i < size; i++){
         bcc2 ^= mensagem[i];
     }
+    printf("0x%.8X",bcc2);
     return bcc2;
 }
 
@@ -136,6 +135,8 @@ int llwrite(int fd, unsigned char *buffer, int length) {
 
     bcc2 = getBCC2(buffer,length);
     bcc2Stuffed = stuffBCC2(bcc2, &sizebcc2);
+
+    printf("0x%.8X 0x%.8X\n",(unsigned)buffer[0],buffer[1]);
 
     
     // Inicio do preenchimento da mensagem
@@ -182,12 +183,12 @@ int llwrite(int fd, unsigned char *buffer, int length) {
     }
     message[i] = FLAG;
 
+
     //Mensagem preenchida Trama I feita
     // printMessage
 
-    puts("Assembling Set message");
     for(int j = 0; j < messageSize; j++){
-        printf("message[%d] = 0x%X\n",j,message[j]);
+        printf("message: 0x%.8X\n",message[j]);
     }
     
 
@@ -198,7 +199,7 @@ int llwrite(int fd, unsigned char *buffer, int length) {
     // Envio da trama
     do {
         // Processo de escrita
-        //tcflush(fd,TCIOFLUSH);
+        tcflush(fd,TCIOFLUSH);
 
         counter++;
 
@@ -246,8 +247,8 @@ int llwrite(int fd, unsigned char *buffer, int length) {
 
     } while(STP || counter < MAXTRIES); //verificar esta condicao
 
-    
-
+    free(message);
+    free(bcc2Stuffed);
     return 0;
 }
 
@@ -327,10 +328,10 @@ int llclose(int fd, int status) {
 
 void alarmHandler(int signo){
 
+  puts("Entered Alarm handler");
   counter++;
   if(counter >= MAXTRIES){
     printf("Exceeded maximum amount of tries: (%d)\n",MAXTRIES);
-    exit(0);
   }
   return ;
 }
