9df1c8d8418f58b355a52d4c2fd6eb0278444f5b
up201806490 <50011557+Hugomguima@users.noreply.github.com>
2020-11-15 18:37:22 +0000

    time1

--- Project1/Emissor/macros.h
+++ Project1/Emissor/macros.h
@@ -38,5 +38,5 @@
 #define MAXTRIES 3
 #define TIMEOUT 1 //Time to wait fora  repsonse from the receiver
 
-#define BCC1ERRORRATE 5
-#define BCC2ERRORRATE 5
+#define BCC1ERRORRATE 0
+#define BCC2ERRORRATE 0
